7543328d2d480a6bb11d659807c96fb86d24fb09
alHaiC0 <AR1056101@wcupa.edu>
2026-08-10 11:06:00 -0400

    Update TestLLMR.java

--- TestLLMR.java
+++ TestLLMR.java
@@ -7,17 +7,10 @@ public class TestLLMR{
       
       linkedTest.add("Head"); 
       linkedTest.add("2nd Node");
-      
-      linkedTest.addHead("New Head!");
-      
       linkedTest.add("3rd Node");
       linkedTest.add("4th Node");
       linkedTest.add("Middle Node");
       linkedTest.add("6th Node");
-      
-      linkedTest.addBetween(10, "In Between?!");
-      linkedTest.addBetween(4, "In Between?!");
-      
       linkedTest.add("7th Node");
       linkedTest.add("8th Node");
       linkedTest.add("Tail Node");
@@ -27,9 +20,11 @@ public class TestLLMR{
       System.out.println(linkedTest.findMiddle());
       
       linkedTest.reverseStack();
+      System.out.println("Reversed with a stack: ");
       System.out.println(linkedTest);
       
       linkedTest.reverseInPlace();
+      System.out.println("Reversed in place: ");
       System.out.println(linkedTest);
       
 
